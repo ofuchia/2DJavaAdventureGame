6a578fcfeb6c4cf8f8ae9a9808f88f3da7d55406
ofuchia <o.akpom@tcu.edu>
2024-09-01 19:28:38 -0500

    able to draw on screen now.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,7 +1,8 @@
 import java.awt.Color;
 import java.awt.Dimension;
-
+import java.awt.Graphics;
 import javax.swing.JPanel;
+import java.awt.Graphics2D;
 
 public class GamePanel extends JPanel implements Runnable {
     // SCREEN SETTINGS
@@ -16,24 +17,41 @@ public class GamePanel extends JPanel implements Runnable {
 
     Thread gameThread;
 
-
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
     }
 
-    public void startGameThread(){
-        gameThread = new Thread(this); //passing in GamepNale class to constructor
-        gameThread.start(); //calls run()
+    public void startGameThread() {
+        gameThread = new Thread(this); // passing in GamepNale class to constructor
+        gameThread.start(); // calls run()
     }
 
-    //creating game loop in run method
+    // creating game loop in run method
     @Override
     public void run() {
-        while(gameThread != null){
-            System.out.println("Game loop running.");
+        while (gameThread != null) {
+            // System.out.println("Game loop running.");
+            // 1. Update info like character position
+            update();
+            // 2. Draw screen
+            repaint(); // how you call paintComponent method
         }
-        
+
+    }
+
+    public void update() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white);
+        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.dispose();
+
     }
 }
