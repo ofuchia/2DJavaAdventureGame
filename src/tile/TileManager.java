e3b13f38c45976de695114b16c733a6e71621c44
ofuchia <o.akpom@tcu.edu>
2024-09-01 22:43:16 -0500

    Tiles are being read correctly. Map is loading.

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -1,7 +1,10 @@
 package tile;
 
 import java.awt.Graphics2D;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import javax.imageio.ImageIO;
 
@@ -10,11 +13,14 @@ import main.GamePanel;
 public class TileManager {
     GamePanel gp;
     Tile[] tile;
+    int mapTileNumber[][];
 
     public TileManager(GamePanel gp) {
         this.gp = gp;
         tile = new Tile[10];
+        mapTileNumber = new int[gp.maxScreenCol][gp.maxScreenRow];
         getTileImage();
+        loadMap("/res/maps/world01.txt");
     }
 
     public void getTileImage() {
@@ -28,8 +34,46 @@ public class TileManager {
             tile[2] = new Tile();
             tile[2].image = ImageIO.read(getClass().getResourceAsStream("/res/tiles/water.png"));
 
+            tile[3] = new Tile();
+            tile[3].image = ImageIO.read(getClass().getResourceAsStream("/res/tiles/earth.png"));
+
+            tile[4] = new Tile();
+            tile[4].image = ImageIO.read(getClass().getResourceAsStream("/res/tiles/tree.png"));
+
+            tile[5] = new Tile();
+            tile[5].image = ImageIO.read(getClass().getResourceAsStream("/res/tiles/sand.png"));
+
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void loadMap(String mapFilePath) {
+        try {
+            InputStream is = getClass().getResourceAsStream(mapFilePath);
+            BufferedReader br = new BufferedReader(new InputStreamReader((is)));
+
+            int col = 0;
+            int row = 0;
+
+            while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
+                String line = br.readLine(); // reads a single line at a time
+
+                while (col < gp.maxScreenCol) {
+                    String numbers[] = line.split(" ");
+                    int num = Integer.parseInt(numbers[col]); // String to int
+                    mapTileNumber[col][row] = num;
+                    col++;
+                }
+                if (col == gp.maxScreenCol) {
+                    col = 0;
+                    row++;
+                }
+            }
+            br.close();
+
+        } catch (Exception e) {
+
         }
     }
 
@@ -41,15 +85,19 @@ public class TileManager {
         int y = 0;
 
         while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
-            g2.drawImage(tile[0].image, x, y, gp.tileSize, gp.tileSize, null);
-            col++;
-            x+= gp.tileSize;
+            System.out.println("Entered drawing");
 
-            if(col == gp.maxScreenCol){
+            int tileNum = mapTileNumber[col][row]; // determins type of tile being drawn after extracting int from txt!
+
+            g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
+            col++;
+            x += gp.tileSize;
+
+            if (col == gp.maxScreenCol) {
                 col = 0;
                 x = 0;
-                row ++;
-                y+= gp.tileSize;
+                row++;
+                y += gp.tileSize;
             }
         }
     }
