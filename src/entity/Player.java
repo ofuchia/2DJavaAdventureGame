d82024a8bf903aa06f5c4e178196d314e6725e6b
ofuchia <o.akpom@tcu.edu>
2024-09-01 22:03:30 -0500

    Sprite no longer walking when idle.

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -43,31 +43,34 @@ public class Player extends Entity {
     }
 
     public void update() {
-        if (keyH.upPressed == true) {
-            direction = "up";
-            y -= speed;
-        } else if (keyH.downPressed == true) {
-            direction = "down";
-            y += speed;
-        } else if (keyH.leftPressed == true) {
-            direction = "left";
-            x -= speed;
-        } else if (keyH.rightPressed == true) {
-            direction = "right";
-            x += speed;
-        }
-
-        spriteCounter++;
-        //player image changes every 12 frames
-        if(spriteCounter > 12){
-            if(spriteNum == 1){
-                spriteNum = 2;
+        if(keyH.upPressed == true || keyH.downPressed == true || keyH.leftPressed == true || keyH.rightPressed == true){
+            if (keyH.upPressed == true) {
+                direction = "up";
+                y -= speed;
+            } else if (keyH.downPressed == true) {
+                direction = "down";
+                y += speed;
+            } else if (keyH.leftPressed == true) {
+                direction = "left";
+                x -= speed;
+            } else if (keyH.rightPressed == true) {
+                direction = "right";
+                x += speed;
             }
-            else if(spriteNum == 2){
-                spriteNum = 1;
+    
+            spriteCounter++;
+            //player image changes every 12 frames
+            if(spriteCounter > 12){
+                if(spriteNum == 1){
+                    spriteNum = 2;
+                }
+                else if(spriteNum == 2){
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
             }
-            spriteCounter = 0;
         }
+        
     }
 
     public void draw(Graphics2D g2) {
